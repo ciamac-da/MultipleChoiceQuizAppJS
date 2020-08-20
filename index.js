a1aa2f8317c9e935285e106099d9253621b6183c
ciamac-da <ciamac.davoudi@gmail.com>
2020-08-20 14:46:46 +0200

    Verified by Cia V1.0.4

---
+++ index.js
@@ -0,0 +1,12 @@
+// select and save all elements in constants
+const start = document.getElementById("start");
+const quiz = document.getElementById("quiz");
+const question = document.getElementById("question");
+const qImg = document.getElementById("qImg");
+const choiceA = document.getElementById("A");
+const choiceB = document.getElementById("B");
+const choiceC = document.getElementById("C");
+const counter = document.getElementById("counter");
+const timeGauge = document.getElementById("timeGauge");
+const progress = document.getElementById("progress");
+const scoreDiv = document.getElementById("scoreContainer");

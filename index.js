0df67d24bffd65227facbe6ff1155680ea52a081
ciamac-da <ciamac.davoudi@gmail.com>
2020-08-20 14:58:21 +0200

    Verified by Cia V1.0.6

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// select and save all elements in constants
+// Select and save all elements in constants
 const start     = document.getElementById("start");
 const quiz      = document.getElementById("quiz");
 const question  = document.getElementById("question");
@@ -10,3 +10,17 @@ const counter   = document.getElementById("counter");
 const timeGauge = document.getElementById("timeGauge");
 const progress  = document.getElementById("progress");
 const scoreDiv  = document.getElementById("scoreContainer");
+
+
+// Here our questions Array!
+let questions = [
+    {
+        question : "What does HTML stand for?",
+        imgsrc   : "img/html.png",
+        choiceA  : "Correct",
+        choiceB  : "Wrong",
+        choiceC  : "Wrong",
+        correct  :  "A"
+
+    }
+];

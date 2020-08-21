4dc5c65dd625728aa0b7d3da4c9ddc07097ab04f
ciamac-da <ciamac.davoudi@gmail.com>
2020-08-21 23:21:38 +0200

    Verified by Cia V1.2.7

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const scoreDiv = document.getElementById("scoreContainer");
 
 // create our questions
 let questions = [
-    {
+    { // html 1
         question : "What type of language HTML is?",
         imgSrc : "img/html.png",
         choiceA : "Markup language",
@@ -22,7 +22,7 @@ let questions = [
         choiceC : "Programming language",
         choiceD : "Network protocol",
         correct : "A"
-    },{
+    },{ // html 2
         question : "Fundamental HTML Block is known as?",
         imgSrc : "img/html.png",
         choiceA : "HTML Element",
@@ -30,7 +30,7 @@ let questions = [
         choiceC : "HTML Body",
         choiceD : "HTML Tag",
         correct : "D"
-    },{
+    },{ // html 3
         question : "Which HTML attribute is used to define inline styles?",
         imgSrc : "img/html.png",
         choiceA : "style",
@@ -38,7 +38,7 @@ let questions = [
         choiceC : "font",
         choiceD : "styles",
         correct : "A"
-    },{
+    },{ // html 4
         question : "Which of the following method is used to get meta information of a resource?",
         imgSrc : "img/html.png",
         choiceA : "top",
@@ -46,7 +46,7 @@ let questions = [
         choiceC : "head",
         choiceD : "grab",
         correct : "C"
-    },{
+    },{ // html 5
         question : "Choose the correct HTML tag for the smallest heading",
         imgSrc : "img/html.png",
         choiceA : "Small heading",
@@ -54,7 +54,7 @@ let questions = [
         choiceC : "Heading small",
         choiceD : "h1",
         correct : "B"
-    },{
+    },{ // html 6
         question : "Where is the correct place to insert a Javascript Code in HTML?",
         imgSrc : "img/html.png",
         choiceA : "Both of C and D",
@@ -62,7 +62,7 @@ let questions = [
         choiceC : "Head section",
         choiceD : "Body section",
         correct : "A"
-    },{
+    },{ // html 7
         question : "The attribute of form tag is:",
         imgSrc : "img/html.png",
         choiceA : "method",
@@ -70,7 +70,7 @@ let questions = [
         choiceC : "None of these",
         choiceD : "action",
         correct : "B"
-    },{
+    },{ // html 8
         question : "Choose the correct HTML element to define emphasized text",
         imgSrc : "img/html.png",
         choiceA : "italic",
@@ -78,7 +78,7 @@ let questions = [
         choiceC : "em",
         choiceD : "strong",
         correct : "C"
-    },{
+    },{ // html 9
         question : "What is the correct HTML element for inserting a line break?",
         imgSrc : "img/html.png",
         choiceA : "br",
@@ -86,7 +86,7 @@ let questions = [
         choiceC : "break",
         choiceD : "hr",
         correct : "A"
-    },{
+    },{ // html 10
         question : "HTML status code is:",
         imgSrc : "img/html.png",
         choiceA : "Three-digit",
@@ -104,22 +104,61 @@ let questions = [
         correct : "C"
     },{ // css 2
         question : "How do you insert a comment in a CSS file?",
-        imgSrc : "img/css.png",
-        choiceA : "// comment",
-        choiceB : "// comment //",
-        choiceC : "' comment",
-        choiceD : "/* comment*/",
-        correct : "D"
+        imgSrc   : "img/css.png",
+        choiceA  : "// comment",
+        choiceB  : "// comment //",
+        choiceC  : "' comment",
+        choiceD  : "/* comment*/",
+        correct  : "D"
     },{ //css 3
-        question : "Which property is used to change the background color??",
+        question : "Which property is used to change the background color?",
         imgSrc : "img/css.png",
         choiceA : "bgcolor",
         choiceB : "background-color",
         choiceC : "color",
         choiceD : "Background-color",
         correct : "B"
-    },
-    {
+    },{ //css 4
+        question : "Which CSS property is used to change the text color of an element?",
+        imgSrc : "img/css.png",
+        choiceA : "color",
+        choiceB : "fgcolor",
+        choiceC : "text-color",
+        choiceD : "text-font",
+        correct : "A"
+    },{ //css 5
+        question : "Which CSS property controls the text size?",
+        imgSrc : "img/css.png",
+        choiceA : "text-size",
+        choiceB : "font-style",
+        choiceC : "text-style",
+        choiceD : "font-size",
+        correct : "D"
+    },{ //css 6
+        question : "Which property is used to change the font of an element?",
+        imgSrc : "img/css.png",
+        choiceA : "font-style",
+        choiceB : "font-weight",
+        choiceC : "font-family",
+        choiceD : "font-background",
+        correct : "C"
+    },{ //css 7
+        question : "How do you make the text bold?",
+        imgSrc : "img/css.png",
+        choiceA : "font-weight:bold;",
+        choiceB : "style:bold;",
+        choiceC : "font:bold;",
+        choiceD : "text:bold;",
+        correct : "A"
+    },{ //css 8
+        question : "How do you select an element with id 'demo'?",
+        imgSrc : "img/css.png",
+        choiceA : ".demo",
+        choiceB : "#demo",
+        choiceC : "demo",
+        choiceD : "*demo",
+        correct : "B"
+    },{
         question : "Which of the following tag is used for javascript?",
         imgSrc : "img/js.png",
         choiceA : "Footer",
